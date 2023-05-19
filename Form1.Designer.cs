27a050b7cff472dda44d00232b41befa391033f7
slowlyraking <72283655+slowlyraking@users.noreply.github.com>
2023-05-19 17:25:48 -0500

    initial file upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace String_Search
+{
+    partial class FileStringSearcher
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStringToSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BrowseFolderButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelShowPath = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxStringToSearch
+            // 
+            this.textBoxStringToSearch.Location = new System.Drawing.Point(96, 137);
+            this.textBoxStringToSearch.Name = "textBoxStringToSearch";
+            this.textBoxStringToSearch.Size = new System.Drawing.Size(158, 23);
+            this.textBoxStringToSearch.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter String to Search below:";
+            // 
+            // BrowseFolderButton
+            // 
+            this.BrowseFolderButton.AutoSize = true;
+            this.BrowseFolderButton.Location = new System.Drawing.Point(100, 12);
+            this.BrowseFolderButton.Name = "BrowseFolderButton";
+            this.BrowseFolderButton.Size = new System.Drawing.Size(150, 25);
+            this.BrowseFolderButton.TabIndex = 2;
+            this.BrowseFolderButton.Text = "Select Folder";
+            this.BrowseFolderButton.UseVisualStyleBackColor = true;
+            this.BrowseFolderButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelShowPath
+            // 
+            this.labelShowPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelShowPath.Location = new System.Drawing.Point(32, 40);
+            this.labelShowPath.Name = "labelShowPath";
+            this.labelShowPath.Size = new System.Drawing.Size(285, 70);
+            this.labelShowPath.TabIndex = 3;
+            this.labelShowPath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(137, 166);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.ItemHeight = 15;
+            this.listBoxResults.Location = new System.Drawing.Point(32, 195);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(285, 274);
+            this.listBoxResults.TabIndex = 5;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(32, 505);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear Form";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(242, 505);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 505);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Save Results";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // FileStringSearcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 541);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelShowPath);
+            this.Controls.Add(this.BrowseFolderButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxStringToSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(350, 580);
+            this.MinimumSize = new System.Drawing.Size(350, 580);
+            this.Name = "FileStringSearcher";
+            this.Text = "Will\'s File String Searcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStringToSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BrowseFolderButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label labelShowPath;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace String_Search
+{
+    public partial class FileStringSearcher : Form
+    {
+        public FileStringSearcher()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                labelShowPath.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+            listBoxResults.Items.Clear();
+            string dirScan = labelShowPath.Text;
+            string filename;
+            string noFileFound = "String Not Found";
+            if (string.IsNullOrWhiteSpace(textBoxStringToSearch.Text) || string.IsNullOrWhiteSpace(dirScan))
+            {
+                MessageBox.Show("Missing Path or Search String");
+                return;
+            }
+            string[] allFiles = System.IO.Directory.GetFiles(dirScan, "*.*");
+            foreach (string file in allFiles)
+            {
+                string[] lines = System.IO.File.ReadAllLines(file);
+                string firstOccurrence = lines.FirstOrDefault(l => l.Contains(textBoxStringToSearch.Text));
+                if (firstOccurrence != null)
+                {
+                    filename = System.IO.Path.GetFileName(file);
+                    listBoxResults.Items.Add(filename);
+                }
+            }
+            if (listBoxResults.Items.Count == 0)
+            {
+                listBoxResults.Items.Add(noFileFound);
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listBoxResults.Items.Clear();
+            textBoxStringToSearch.Text = "";
+            labelShowPath.Text = "";
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            const string sPath = "save.txt";
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach(var item in listBoxResults.Items)
+            {
+                SaveFile.WriteLine(item.ToString());
+            }
+            SaveFile.Close();
+
+            MessageBox.Show("Results Saved!");
+        }
+    }
+}
